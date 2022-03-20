4a89f9a20c2f1ac04f8e249bbc706c6fcebfc183
Shilohiy <109141425@qq.com>
2022-03-20 14:26:21 +0800

    master

--- letter.cpp
+++ letter.cpp
@@ -1,12 +1,15 @@
-#include<stdio.h>
-int main(){
-	int Let[26],i;
-	Let[0]='a';
-	for(i=1;i<26;i++){
-		Let[i]=Let[0]+i;
+#include <stdio.h>
+int main()
+{
+	int Let[26], i;
+	Let[0] = 'a';
+	for (i = 1; i < 26; i++)
+	{
+		Let[i] = Let[0] + i;
 	}
-	for(i=0;i<26;i++){
-		printf("%c ",Let[i]);
+	for (i = 0; i < 26; i++)
+	{
+		printf("%c ", Let[i]);
 	}
 	return 0;
-} 
+}
